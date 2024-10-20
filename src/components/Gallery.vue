39c34e314bbcd2f81b559265de8a1b4f8954b757
Munees <muneesmmm@gmail.com>
2024-10-20 22:48:00 +0530

    fix

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -6,7 +6,7 @@
                 <div class="swiper-wrapper h-full max-h-[600px]">
                     <div v-for="(image, index) in gallery" :key="index" data-aos="fade-left" class="swiper-slide flex justify-center">
                         <div class="bg-white shadow-md rounded-3xl flex flex-col items-center h-fit">
-                            <img :src="getImageUrl(image)" alt="Premiumdentalstudio gallery" class="rounded-3xl w-full max-w-[416px]" />
+                            <img :src="getImageUrl(image)" alt="Premium dental studio gallery" class="rounded-3xl w-full max-w-[416px]" />
                         </div>
                     </div>
                 </div>
