a373ef75b3498fb563ae5fc91d33656b6eeb2fd1
Munees <muneesmmm@gmail.com>
2024-11-13 01:24:51 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,6 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/',
-  plugins: [vue()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://maps.googleapis.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/maps/api'), // Ensure path starts with /maps/api
-      },
-    },
-  },  
-  
+  base: '/',
+  plugins: [vue()], 
 });
